00ace904106bc046148158340cdfece2281f67f3
Muhammad Afaq Shuaib <afaq.shoaib09@gmail.com>
2021-11-07 17:59:44 +0500

    log statements added to check activity life cycle

--- app/src/main/java/com/example/counterappwithsplashscreen/HomeActivity.java
+++ app/src/main/java/com/example/counterappwithsplashscreen/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,9 +13,11 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     Button btnInc, btnDec, btnReset;
     TextView textView;
+    public static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, " onCreate Activity Main");
         setContentView(R.layout.activity_home);
 
         btnInc = findViewById(R.id.btn_inc);
@@ -47,5 +50,34 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_reset:
                 textView.setText("0");
         }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart Activity Main");
+    }
+
+    @Override
+    protected void onResume() {
+        super. onResume();
+        Log.d(TAG, " onResume Activity Main");
+    }
+
+    @Override
+    protected void onPause() {
+        super. onPause();
+        Log.d(TAG, " onPause Activity Main");
+    }
+
+    @Override
+    protected void onStop() {
+        super. onStop();
+        Log.d(TAG, " onStop Activity Main");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super. onDestroy();
+        Log.d(TAG, " onDestroy Activity Main");
     }
 }
